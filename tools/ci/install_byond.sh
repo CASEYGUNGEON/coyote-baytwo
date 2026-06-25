0a70fd9e8e3d35390e103860c3f9e29d4f9b58a3
CASEY GUNGEON <bound2theground@gmail.com>
2026-06-24 20:20:25 -0700

    no sudo

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 source dependencies.sh
 
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt install libcurl4:i386
+dpkg --add-architecture i386
+apt-get update
+apt install libcurl4:i386
 
 if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
 then
