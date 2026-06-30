c7f20b9f9a19794d0f9e587eb8a0f178941b17a6
CASEY GUNGEON <bound2theground@gmail.com>
2026-06-29 17:37:48 -0700

    update python ver

--- dependencies.sh
+++ dependencies.sh
@@ -18,7 +18,7 @@ export NODE_VERSION_PRECISE=18.13.0
 export SPACEMAN_DMM_VERSION=suite-1.8
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.7.9
+export PYTHON_VERSION=3.12.3
 
 # Auxmos git tag
 export AUXMOS_VERSION=v2.5.6
